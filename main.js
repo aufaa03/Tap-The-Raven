c5c6858aa66a9e29835de8a5cb21e2f3e5ec73dc
aufaa03 <142073902+aufaa03@users.noreply.github.com>
2025-01-12 19:28:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
 // canvas.style.cursor = "url('../img/cursor.png') auto, default";
